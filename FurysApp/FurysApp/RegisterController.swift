d9c1f6817d03d11584efaf5638f8618a3e488326
Dion MacIntyre <dion@macintyres.me.uk>
2017-03-26 22:21:53 +0100

    Implented Register functionality
    Can register through app, checks for empty fields and for password == to confirmpassword

---
+++ FurysApp/FurysApp/RegisterController.swift
@@ -0,0 +1,21 @@
+//
+//  RegisterController.swift
+//  FurysApp
+//
+//  Created by Dion MacIntyre on 25/03/2017.
+//  Copyright © 2017 Dion MacIntyre. All rights reserved.
+//
+
+import UIKit
+
+class RegisterController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
