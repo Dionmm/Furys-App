83c8b324f466780ae2e865c3400f68a86be424e3
Dion MacIntyre <dion@macintyres.me.uk>
2017-03-28 18:55:01 +0100

    Added loginController, SelectionController
    Made some changes to improve UI, store user details in UserDefaults and began post-login functionality

---
+++ FurysApp/FurysApp/LoginController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginController.swift
+//  FurysApp
+//
+//  Created by Dion MacIntyre on 28/03/2017.
+//  Copyright © 2017 Dion MacIntyre. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
